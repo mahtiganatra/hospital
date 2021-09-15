8ddf906f2a00f49b708eb558910d7bd7d6ab9bf4
mahtiganatra <mahtiganatra@gmail.com>
2021-09-16 01:56:57 +0530

    added all the updates cards on the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,6 +10,14 @@ import IconButtons from '../Components/HomeIcons';
 import OutlinedCard1 from '../Components/BottomCard';
 import ProminentAppBar from '../Components/HomeBar';
 import SimpleList from '../Components/HomeList';
+import SwipeableTextMobileStepper from '../Components/HomeCar1';
+import BasicTextFields from '../Components/UpdatesTxt';
+import OutlinedCard4 from '../Components/UpdatesCard1';
+import OutlinedCard5 from '../Components/UpdatesCard2';
+import OutlinedCard6 from '../Components/UpdatesCard3';
+import OutlinedCard7 from '../Components/UpdatesCard4';
+import OutlinedCard8 from '../Components/UpdatesCard5';
+import OutlinedCard9 from '../Components/UpdatesCard6';
 
 
 export default function Home() {
@@ -23,6 +31,15 @@ export default function Home() {
 {/* <IconButtons/> */}
 {/* <OutlinedCard1/> */}
 {/* <SimpleList/> */}
+<SwipeableTextMobileStepper/>
+<BasicTextFields/>
+<OutlinedCard4/>
+<OutlinedCard5/>
+<OutlinedCard6/>
+<OutlinedCard7/>
+<OutlinedCard8/>
+<OutlinedCard9/>
+
 
            
            
